236cd451766506a81f2b6f22e8c97bdfe4bc66e4
Rjrazu <razuahmed00042@gmail.com>
2021-10-04 00:40:48 +0600

    first commit

---
+++ src/componants/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h2>Hi From Header</h2>
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/componants/Service/Service.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Service = () => {
+    return (
+        <div>
+            <h2>Hello From Service</h2>
+        </div>
+    );
+};
+
+export default Service;
